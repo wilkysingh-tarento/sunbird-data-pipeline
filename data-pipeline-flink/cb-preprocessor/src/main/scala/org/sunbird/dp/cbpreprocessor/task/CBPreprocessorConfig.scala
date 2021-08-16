29b3dcf2cb9c1b392d2bb038850a3162db424cb0
Shobhit Vashistha <shobhit.vashistha@tarento.com>
2021-08-16 22:19:58 +0530

    more editd

--- data-pipeline-flink/cb-preprocessor/src/main/scala/org/sunbird/dp/cbpreprocessor/task/CBPreprocessorConfig.scala
+++ data-pipeline-flink/cb-preprocessor/src/main/scala/org/sunbird/dp/cbpreprocessor/task/CBPreprocessorConfig.scala
@@ -27,30 +27,17 @@ class CBPreprocessorConfig(override val config: Config) extends BaseJobConfig(co
   val kafkaLogRouteTopic: String = config.getString("kafka.output.log.route.topic")
   val kafkaErrorRouteTopic: String = config.getString("kafka.output.error.route.topic")
   val kafkaAuditRouteTopic: String = config.getString("kafka.output.audit.route.topic")
-  //val kafkaCbAuditRouteTopic: String = config.getString("kafka.output.cb.audit.route.topic")
-  val kafkaCbAuditRouteTopic: String = "dev.telemetry.cb.audit"
+  val kafkaCbAuditOutputRouteTopic: String = config.getString("kafka.output.cb.audit.druid.route.topic")
 
   val kafkaFailedTopic: String = config.getString("kafka.output.failed.topic")
   val kafkaDuplicateTopic: String = config.getString("kafka.output.duplicate.topic")
-
-  val kafkaDenormSecondaryRouteTopic: String = config.getString("kafka.output.denorm.secondary.route.topic")
-  val kafkaDenormPrimaryRouteTopic: String = config.getString("kafka.output.denorm.primary.route.topic")
 
   val defaultChannel: String = config.getString("default.channel")
 
   val includedProducersForDedup: List[String] = config.getStringList("dedup.producer.included.ids").asScala.toList
 
-  // Validation & dedup Stream out put tag
-  val validationFailedEventsOutputTag: OutputTag[Event] = OutputTag[Event]("validation-failed-events")
-  val uniqueEventsOutputTag: OutputTag[Event] = OutputTag[Event]("unique-events")
-  val duplicateEventsOutputTag: OutputTag[Event] = OutputTag[Event]("duplicate-events")
-
   // Router stream out put tags
-  val primaryRouteEventsOutputTag: OutputTag[Event] = OutputTag[Event]("primary-route-events")
-
-  // Spliting events on priority for faster denorm processing
-  val denormSecondaryEventsRouteOutputTag: OutputTag[Event] = OutputTag[Event]("denorm-secondary-events")
-  val denormPrimaryEventsRouteOutputTag: OutputTag[Event] = OutputTag[Event]("denorm-primary-events")
+  // val primaryRouteEventsOutputTag: OutputTag[Event] = OutputTag[Event]("primary-route-events")
 
   // Audit, Log & Error Events output tag
   val auditRouteEventsOutputTag: OutputTag[Event] = OutputTag[Event]("audit-route-events")
@@ -58,17 +45,8 @@ class CBPreprocessorConfig(override val config: Config) extends BaseJobConfig(co
   val logEventsOutputTag: OutputTag[Event] = OutputTag[Event]("log-route-events")
   val errorEventOutputTag: OutputTag[Event] = OutputTag[Event]("error-route-events")
 
-  // Share events out put tags
-  val shareRouteEventsOutputTag: OutputTag[Event] = OutputTag[Event]("share-route-events")
-  val shareItemEventOutputTag: OutputTag[Event] = OutputTag[Event]("share-item-events")
-
   override val kafkaConsumerParallelism: Int = config.getInt("task.consumer.parallelism")
   val downstreamOperatorsParallelism: Int = config.getInt("task.downstream.operators.parallelism")
-
-  val VALIDATION_FLAG_NAME = "pp_validation_processed"
-  val DEDUP_FLAG_NAME = "pp_duplicate"
-  val DEDUP_SKIP_FLAG_NAME = "pp_duplicate_skipped"
-  val SHARE_EVENTS_FLATTEN_FLAG_NAME = "pp_share_event_processed"
 
   // Router job metrics
   val primaryRouterMetricCount = "primary-route-success-count"
@@ -114,8 +92,5 @@ class CBPreprocessorConfig(override val config: Config) extends BaseJobConfig(co
   val denormPrimaryEventProducer = "denorm-primary-events-sink"
 
   val defaultSchemaFile = "envelope.json"
-
-  val secondaryEvents: List[String] = config.getStringList("secondary.events").asScala.toList
-
 
 }
